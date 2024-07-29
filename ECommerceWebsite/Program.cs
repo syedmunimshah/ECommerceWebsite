4b8c030ceeb85e314393fdb0e91d24901872efeb
Syed Abdul Munim Ali Shah <syedmuuni@gmail.com>
2024-07-30 03:51:22 +0500

    save changes

--- ECommerceWebsite/Program.cs
+++ ECommerceWebsite/Program.cs
@@ -32,6 +32,6 @@ app.UseSession();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Admin}/{action=Index}/{id?}");
+    pattern: "{controller=Customer}/{action=Index}/{id?}");
 
 app.Run();
